ec62e73c427ca54360f4206bc07a7d5d5f170427
Anthony Harris <33495441+KillerBOB999@users.noreply.github.com>
2019-11-19 12:06:16 -0600

    Finished C# implementation of KNN

--- CSharpParkingSpaceClassification/Program.cs
+++ CSharpParkingSpaceClassification/Program.cs
@@ -6,53 +6,111 @@ namespace CSharpParkingSpaceClassification
 {
     class Program
     {
-        static List<Tuple<List<float>, int, int>> trainingDataSet = new List<Tuple<List<float>, int, int>>();
-        static List<Tuple<List<float>, int, int>> testDataSet = new List<Tuple<List<float>, int, int>>();
-        static int K_NumberOfNeighbors = 10;
+        static List<Tuple<List<double>, int>> trainingDataSet = new List<Tuple<List<double>, int>>();
+        static List<Tuple<List<double>, int>> testDataSet = new List<Tuple<List<double>, int>>();
+        static List<int> classifications = new List<int>();
+        static int K_NumberOfNeighbors;
         static int power = 2;
         static int numCorrect = 0;
         static int numIncorrect = 0;
 
-        static float DistanceMeasure(Tuple<List<float>, int, int> entry1, Tuple<List<float>, int, int> entry2)
+        static double DistanceMeasure(Tuple<List<double>, int> entry1, Tuple<List<double>, int> entry2)
 		{
-			float currentSum = 0;
-			for (int index = 0; index < entry1[0].Count; ++index)
+			double currentSum = 0;
+			for (int index = 0; index < entry1.Item1.Count; ++index)
 			{
-				currentSum += Math.Pow(entry1[0][index] - entry2[0][index], power);
+				currentSum += Math.Pow(Math.Abs(entry1.Item1[index] - entry2.Item1[index]), power);
 			}
-			return Math.Pow(currentSum, 0.5);
+			return (double)Math.Sqrt(currentSum);
 		}
 
-        static List<Tuple<int, float>> FindNeighbors(Tuple<List<float>, int, int> resident)
+        static List<Tuple<int, double>> FindNeighbors(Tuple<List<double>, int> resident)
         {
-            List<Tuple<int, float>> neighbors = new List<Tuple<int, float>>();
+            List<Tuple<int, double>> neighbors = new List<Tuple<int, double>>();
 
             for (int index = 0; index < trainingDataSet.Count; ++index)
             {
                 if (neighbors.Count < K_NumberOfNeighbors)
                 {
-                    neighbors.Add(new Tuple<int, float>(index, DistanceMeasure(trainingDataSet[index], resident)));
+                    neighbors.Add(new Tuple<int, double>(index, DistanceMeasure(trainingDataSet[index], resident)));
                 }
-                // KEEP GOING HERE
+                else
+                {
+                    double currentDistance = DistanceMeasure(trainingDataSet[index], resident);
+                    Tuple<int, double> currentFarthestNeighbor = neighbors[0];
+                    foreach (var neighbor in neighbors)
+                    {
+                        if (neighbor.Item2 > currentFarthestNeighbor.Item2)
+                        {
+                            currentFarthestNeighbor = neighbor;
+                        }
+                    }
+                    for (int neighborIndex = 0; neighborIndex < neighbors.Count; ++neighborIndex)
+                    {
+                        if (neighbors[neighborIndex].Item2 == currentFarthestNeighbor.Item2 && currentDistance < currentFarthestNeighbor.Item2)
+                        {
+                            neighbors.RemoveAt(neighborIndex);
+                            neighbors.Add(new Tuple<int, double>(index, currentDistance));
+                        }
+                    }
+                }
             }
-
             return neighbors;
+        }
+
+        static void PrintData(ref int index)
+        {
+            Console.WriteLine("Desired Class: " + testDataSet[index].Item2.ToString() +
+                                "\tComputed Class: " + classifications[index].ToString());
         }
 
         static void Classify()
         {
-            foreach (var entry in testDataSet)
+            for (int entry = 0; entry < testDataSet.Count; ++entry)
             {
-                List<List<float>> classVote = new List<List<float>>()
+                List<List<double>> classVote = new List<List<double>>()
                 {
-                    new List<float>(){0, 0},
-                    new List<float>(){1, 0}
+                    new List<double>(){0, 0},
+                    new List<double>(){1, 0},
+                    new List<double>(){2, 0},
+                    new List<double>(){3, 0},
+                    new List<double>(){4, 0},
+                    new List<double>(){5, 0},
+                    new List<double>(){6, 0},
+                    new List<double>(){7, 0},
+                    new List<double>(){8, 0},
+                    new List<double>(){9, 0},
                 };
-                List<Tuple<int, float>> neighbors = FindNeighbors(entry);
+                List<Tuple<int, double>> neighbors = FindNeighbors(testDataSet[entry]);
+
+                foreach (var neighbor in neighbors)
+                {
+                    foreach (var classLabel in classVote)
+                    {
+                        if (classLabel[0] == trainingDataSet[neighbor.Item1].Item2) classLabel[1] += 1 / neighbor.Item2;
+                    }
+                }
+
+                double maxVoteWeight = double.MinValue;
+                int finalVote = -1;
+                foreach (var vote in classVote)
+                {
+                    if (vote[1] > maxVoteWeight)
+                    {
+                        maxVoteWeight = vote[1];
+                        finalVote = (int)vote[0];
+                    }
+                }
+                classifications.Add(finalVote);
+
+                if (finalVote == testDataSet[entry].Item2) ++numCorrect;
+                else ++numIncorrect;
+
+                PrintData(ref entry);
             }
         }
 
-        static void CollectData(string fileName, ref List<Tuple<List<float>, int, int>> dataSet, int sizeOfDataSet)
+        static void CollectData(string fileName, ref List<Tuple<List<double>, int>> dataSet, int sizeOfDataSet, bool startFromBeginning)
         {
             System.IO.DirectoryInfo root = new DirectoryInfo(@"./../../../" + fileName);
             System.IO.FileInfo[] files = null;
@@ -63,56 +121,60 @@ namespace CSharpParkingSpaceClassification
             {
                 int entryCounter = 0;
                 files = dir.GetFiles();
+                if (!startFromBeginning) Array.Reverse(files);
                 foreach (var file in files)
+                if (entryCounter < sizeOfDataSet / 2)
                 {
-                    if (entryCounter < sizeOfDataSet / 2)
+                    using (var reader = new StreamReader(file.ToString()))
                     {
-                        using (var reader = new StreamReader(file.ToString()))
+                        var data = reader.ReadLine();
+                        var values = data.Split(',');
+                        List<double> pixels = new List<double>();
+                        for (int i = 1; i < values.Length; ++i)
                         {
-                            var data = reader.ReadLine();
-                            var values = data.Split(',');
-                            List<float> pixels = new List<float>();
-                            for (int i = 1; i < values.Length; ++i)
-                            {
-                                pixels.Add(float.Parse(values[i]));
-                            }
-                            int actual = -1;
-                            if (values[0] == "Available")
-                            {
-                                actual = 1;
-                            }
-                            else
-                            {
-                                actual = 0;
-                            }
-                            dataSet.Add(new Tuple<List<float>, int, int>(pixels, actual, -1));
+                            pixels.Add(double.Parse(values[i]));
                         }
-                        entryCounter++;
+                        int actual = -1;
+                        if (values[0] == "Available")
+                        {
+                            actual = 1;
+                        }
+                        else
+                        {
+                            actual = 0;
+                        }
+                        dataSet.Add(new Tuple<List<double>, int>(pixels, actual));
                     }
+                    entryCounter++;
                 }
             }
         }
         static void Main(string[] args)
         {
             Console.WriteLine("Collecting Training Data, Please Wait. . .");
-            CollectData("CNRPARK-Patches-150x150-Grayscale/A", ref trainingDataSet, 1000);
+            CollectData("CNRPARK-Patches-150x150-Grayscale/A", ref trainingDataSet, 1000, true);
             Console.WriteLine("Training Data Collection Successful.");
 
             Console.WriteLine();
 
             Console.WriteLine("Collecting Testing Data, Please Wait. . .");
-            CollectData("CNRPARK-Patches-150x150-Grayscale/B", ref testDataSet, 100);
+            CollectData("CNRPARK-Patches-150x150-Grayscale/A", ref testDataSet, 100, false);
             Console.WriteLine("Testing Data Collection Successful.");
 
             Console.WriteLine();
 
+            K_NumberOfNeighbors = (int)(Math.Sqrt(trainingDataSet.Count) / 4);
             Console.WriteLine("Now beginning display of KNN algorithm information:");
             Console.WriteLine("K = " + K_NumberOfNeighbors.ToString());
+
             Console.WriteLine();
+            
             Classify();
+            
             Console.WriteLine();
+            
             int total = numCorrect + numIncorrect;
-            float accuracy = (float)numCorrect / (float)numIncorrect;
+            double accuracy = (double)numCorrect / (double)total;
             Console.WriteLine("Accuracy: " + accuracy.ToString());
             Console.WriteLine("Number correctly classified: " + numCorrect.ToString());
             Console.WriteLine("Number incorrectly classified: " + numIncorrect.ToString());
